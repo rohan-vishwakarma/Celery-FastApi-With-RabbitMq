02dd2e239d0bd0d8674345d43af3e6f154507bd8
rohan-vishwakarma <rohanvishwakarma685@gmail.com>
2024-03-27 12:37:01 +0530

    finally

--- app/CeleryApp/__init__.py
+++ app/CeleryApp/__init__.py
@@ -2,14 +2,29 @@ from celery import Celery
 import time
 from celery.utils.log import get_task_logger
 from app.CeleryApp.celeryconfig import *
+from celery.schedules import crontab
 
 celery = Celery('tasks', broker='pyamqp://guest:guest@localhost//', backend='redis://localhost:6379')
 celery.config_from_object('app.CeleryApp.celeryconfig')
+celery.conf.beat_schedule = {
+    'add-every-minutes': {
+            'task': 'app.CeleryApp.schedule_job',
+            'schedule': crontab(),
+            'args': ("hello", "rohan")
+        },
+}
+
 logger = get_task_logger(__name__)
 
-
 @celery.task()
-def addition(num1, num2):
+def addition(num1, num2 ):
     logger.info("TASK STARTED EXECUTING FOR ADDITION OF PROGRAM")
     time.sleep(10)
+    return num1 + num2
+
+@celery.task
+def schedule_job(num1, num2) -> str:
+    logger.info("CELERY BEAT STARTED")
+    time.sleep(5)
+    logger.info("Celery task Complete")
     return num1 + num2
